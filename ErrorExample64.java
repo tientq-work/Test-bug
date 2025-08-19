78dcbfb6a17083b410e8c3ab01c6efcc30929e8c
tientq-work <133194662+tientq-work@users.noreply.github.com>
2025-08-20 01:34:49 +0700

    java bi loi

---
+++ ErrorExample64.java
@@ -0,0 +1,13 @@
+public class ErrorExample64 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java 64-bit error!")
+        int[] arr = new int[2];
+        arr[5] = 10;
+        String s = null;
+        System.out.println(s.toUpperCase());
+        int x = "abc";
+        for(int i = 0; i < 3; i++)
+            System.out.println(i)
+        undeclaredMethod();
+    }
+}
