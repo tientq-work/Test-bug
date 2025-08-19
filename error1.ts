d0f8ef70fdae3ecc937c37e7b92d12ab1ca3358c
tientq-work <133194662+tientq-work@users.noreply.github.com>
2025-08-20 01:38:33 +0700

    CODE TS LOI

---
+++ error1.ts
@@ -0,0 +1,8 @@
+// File TypeScript lỗi 1
+function add(a: number, b: string): number {
+    return a + b;
+}
+
+let result = add(5, 10);
+console.log(reslt);
+undeclaredFunction();
---
+++ error2.ts
@@ -0,0 +1,16 @@
+// File TypeScript lỗi 2
+interface Person {
+    name: string;
+    age: number;
+}
+
+let p: Person = {
+    name: 123,
+    age: "twenty"
+};
+
+console.log(p.nam);
+function test() {
+    return x + 1;
+}
+test();
