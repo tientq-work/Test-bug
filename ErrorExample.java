48055f1f1216601558b12e735db747a665d2519d
tientq-work <133194662+tientq-work@users.noreply.github.com>
2025-08-19 23:40:15 +0700

    Update ErrorExample.java

--- ErrorExample.java
+++ ErrorExample.java
@@ -1,5 +1,10 @@
 public class ErrorExample {
     public static void main(String[] args) {
         System.out.println("Hello, world!"
+        int x = "string instead of int";
+        undeclaredMethod();
+        for(int i = 0; i < 5; i++) {
+            System.out.println(i)
+        }
     }
 }
