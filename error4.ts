a0e93c397033c5c5401d869e98ad488b9cdaa6b0
tientq-work <133194662+tientq-work@users.noreply.github.com>
2025-08-20 02:45:23 +0700

    Create error4.ts

---
+++ error4.ts
@@ -0,0 +1,25 @@
+// File TypeScript lỗi 4
+function divide(a: number, b: number): number {
+    return a / b;
+}
+
+let result = divide("10", 0);
+console.log(reslt);
+let arr: string[] = ["a", "b", 3];
+arr.push(4);
+let obj = { name: "Bob" };
+console.log(obj.age.toFixed());
+function test2(x: string): number {
+    return x * 2;
+}
+let y = test2(5);
+let z: boolean = 123;
+console.log(z.toUpperCase());
+let n: number = undefined;
+n.toFixed();
+let s: string = null;
+console.log(s.length);
+let arr2: number[] = undefined;
+arr2.push(1);
+let foo = bar;
+foo();
