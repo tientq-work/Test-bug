15bc3dc5d3b3ee3561e4343ff9b6b4b20155ede7
tientq-work <133194662+tientq-work@users.noreply.github.com>
2025-08-19 23:54:47 +0700

    Create errorExample.js

---
+++ errorExample.js
@@ -0,0 +1,14 @@
+// File JavaScript có lỗi
+function errorExample() {
+    console.log("Hello, world!"
+    let x = "string;
+    y = x + 1;
+    undeclaredFunction();
+    if(true)
+        console.log("Missing brace")
+    arr = [1,2,3];
+    console.log(arr[10]);
+    let z;
+    z++;
+}
+errorExample();
